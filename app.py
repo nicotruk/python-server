156f1def376fb6a29c8e5e71eef9fde58f548af0
Nico Truk <nicotruk@gmail.com>
2018-03-29 14:19:59 -0300

    pruebas para integracion con mongodb

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
 from flask import Flask
+from flask_pymongo import PyMongo
 from flask_restful import Api
+from model.user import User
 
-
-app = Flask(__name__)
+app = Flask("python_server")
 
 api = Api(app, prefix="/api/v1")
+
+# connect to another MongoDB database on the same host
+app.config['MONGO_DBNAME'] = 'python_server'
+mongo = PyMongo(app, config_prefix='MONGO')
 
 
 @app.route('/')
 def hello_world():
+    usersCount = mongo.db.users.count()
+    print "Users count : {}".format(usersCount)
+    user = User("1", "nico", "truksinas")
+
+    mongo.db.users.insert(user.encode_user())
+    usersCount = mongo.db.users.count()
+    print "Users count : {}".format(usersCount)
     return "Hi, I'm root!"
 
 
