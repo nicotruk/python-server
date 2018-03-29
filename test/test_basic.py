7d60281e2a763555233fd3d829c006d2b57fb229
gmerino92 <merino.gonza@gmail.com>
2018-03-29 14:41:17 -0300

    New test

--- test/test_basic.py
+++ test/test_basic.py
@@ -3,6 +3,5 @@ import unittest
 
 class BasicTestCase(unittest.TestCase):
 
-	@given(st.integers(), st.integers())
-	def test_ints_are_commutative(x, y):
-    	assert x + y == y + x
+	def test_basic():
+    	assert 1 == 1
