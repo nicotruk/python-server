0c823099516850bdc9c4c7df6b1ec2956b67db9e
gmerino92 <merino.gonza@gmail.com>
2018-04-02 02:10:02 -0300

    Test

--- test/test_cat_resource.py
+++ test/test_cat_resource.py
@@ -1,3 +1,6 @@
+import sys, os
+myPath = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, myPath + '/../')
 from resources.cat_resource import CatResource
 from cats import cats
 import unittest
