3ba68aa42375d3142984ce7c7d45068fce18ed00
gmerino92 <merino.gonza@gmail.com>
2018-04-02 01:16:30 -0300

    Test coverage #2

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_restful import Api
 
+from resources.cat_resource import CatResource
+
 
 app = Flask(__name__)
 
 api = Api(app, prefix="/api/v1")
 
+api.add_resource(CatResource, '/cats')
 
 @app.route('/')
 def hello_world():
