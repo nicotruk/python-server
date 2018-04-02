c23d912400000e65d94887cb38d5765b0b6aef96
gmerino92 <merino.gonza@gmail.com>
2018-04-02 02:34:56 -0300

    test

--- test/test_cat_resource.py
+++ test/test_cat_resource.py
@@ -1,6 +1,8 @@
+#Lo siguiente debe inicializarse en setup.py
 import sys, os
 myPath = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, myPath + '/../')
+########
 from resources.cat_resource import CatResource
 from cats import cats
 import unittest
@@ -10,3 +12,14 @@ class CatResourceTestCase(unittest.TestCase):
   def test_get_cats(self):
     service = CatResource()
     self.assertEqual(service.get(), cats)
+
+  def test_post_cat(self):
+  	service = CatResource()
+  	cat = {
+      "name": "Trigger",
+      "color": "orange",
+      "owner": None,
+      "weight": "5kg"
+    }
+  	#service.post({})
+  	self.assertIn(cat, cats)
