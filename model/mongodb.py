4770e60d944062536004436c154e39514333243e
gmerino92 <merino.gonza@gmail.com>
2018-04-17 16:39:58 -0300

    Setting ENV variables v1

--- model/mongodb.py
+++ model/mongodb.py
@@ -1,7 +1,9 @@
 import os
+import pprint
 from pymongo import MongoClient
 
-mongo_uri = "mongodb://grupo2:123@ds249269.mlab.com:49269/heroku_5d6zh6jz"
+#mongo_uri = "mongodb://grupo2:123@ds249269.mlab.com:49269/heroku_5d6zh6jz"
+mongo_uri = os.environ['DB_PORT_27017_TCP_ADDR']+":"+os.environ['MONGO_PORT']
 
 mongo = MongoClient(mongo_uri)
 db = mongo['heroku_5d6zh6jz']
