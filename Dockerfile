aa8ce68065504bed914bf4458a51ac167e92e48e
gmerino92 <merino.gonza@gmail.com>
2018-04-17 02:07:31 -0300

    Attempt to fix Travis

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,6 @@ ADD . /python-server
 
 WORKDIR /python-server
 
-EXPOSE 8000
-
 RUN pip install --upgrade pip
 
 RUN pip install -r requirements.txt
--- model/mongodb.py
+++ model/mongodb.py
@@ -1,5 +1,5 @@
 import os
 from pymongo import MongoClient
 
-mongo = MongoClient(os.environ['DB_PORT_27017_TCP_ADDR'],27017)
+mongo = MongoClient(os.environ['MONGO_URL'],27017)
 db = mongo.python_server
