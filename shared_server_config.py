d66e42d351f353ad2d1a9d981fb7a8134ebc5d25
Nico Truk <nicotruk@gmail.com>
2018-05-01 19:34:48 -0300

    mongo config> de local a heroku

--- shared_server_config.py
+++ shared_server_config.py
@@ -1,4 +1,4 @@
-SHARED_SERVER_URI = 'https://stories-shared-server.herokuapp.com'
+SHARED_SERVER_URI = 'https://shared-server-stories.herokuapp.com'
 SHARED_SERVER_PING_PATH = SHARED_SERVER_URI + '/api/ping'
 SHARED_SERVER_USER_PATH = SHARED_SERVER_URI + '/api/user'
 SHARED_SERVER_TOKEN_PATH = SHARED_SERVER_URI + '/api/token'
