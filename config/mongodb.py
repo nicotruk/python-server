8a93c3347a87df09114bb674a19e836a0037c2a7
gmerino92 <merino.gonza@gmail.com>
2018-05-05 17:12:51 -0300

    Adjusted VOs and added UserVO. Also modified mongo config file to check for env variable.

--- config/mongodb.py
+++ config/mongodb.py
@@ -1,10 +1,10 @@
 import os
 from pymongo import MongoClient
 
-# mongo_uri = os.environ['MONGO_URL'] + ":" + os.environ['MONGO_PORT']
-
-# Para correr el comando pytest localmente, comentar la linea anterior y descomentar la siguiente.
-mongo_uri = "mongodb://localhost:27017"
+if "MONGO_URL" in os.environ:
+	mongo_uri = os.environ['MONGO_URL'] + ":" + os.environ['MONGO_PORT']
+else:
+	mongo_uri = "mongodb://localhost:27017"
 
 mongo = MongoClient(mongo_uri)
 db = mongo['heroku_5d6zh6jz']
---
+++ model/db/userVO.py
@@ -0,0 +1,10 @@
+class UserVO:
+
+    def __init__(self, user_id, username, email, first_name, last_name, profile_pic, friends):
+        self.user_id = user_id
+        self.username = username
+        self.email = email
+        self.first_name = first_name
+        self.last_name = last_name
+        self.profile_pic = profile_pic
+        self.friends_ids = friends_ids
