e4d3acc8efde8e5cad4b7bef90d069276b7f8414
gocasey <gonzak6@gmail.com>
2018-05-17 14:33:40 -0300

    Initial work for file upload

--- resources/story_resource.py
+++ resources/story_resource.py
@@ -5,7 +5,7 @@ import requests
 from flask import request, jsonify, make_response
 from flask_restful import Resource
 
-from config.shared_server_config import SHARED_SERVER_USER_PATH, SHARED_SERVER_TOKEN_PATH, SHARED_SERVER_APPLICATION_OWNER
+from config.shared_server_config import SHARED_SERVER_FILE_UPLOAD_PATH
 from model.story import Story
 from resources.error_handler import ErrorHandler
 
@@ -27,23 +27,32 @@ class StoriesResource(Resource):
             logging.info("Received StoriesResource POST Request")
             story_data = json.loads(request.data)
 
-            # TODO: generate fileURL based on info retrieved by Androd
-            # and a call to the shared server to save the blob
-            file_url = ''
-
-            story_created = Story.create(
-                story_data["user_id"],
-                story_data["location"],
-                story_data["visibility"],
-                story_data["title"],
-                story_data["description"],
-                file_url,
-                story_data["is_quick_story"],
-                story_data["timestamp"]
-            )
-            logging.debug("Python Server Response: 201 - %s", story_created)
-            return make_response(jsonify(story_created), 200)
+            payload = {
+                "file": story_data["uploaded_file"],
+                "metadata": {
+                    "name": story_data["uploaded_filename"]
+                }
+            }
+            headers = {'content-type': 'application/json'}
+            response = requests.post(SHARED_SERVER_FILE_UPLOAD_PATH, data=json.dumps(payload), headers=headers)
+            file_data = json.loads(response.text)
+            if response.ok:
+                story_created = Story.create(
+                    story_data["user_id"],
+                    story_data["location"],
+                    story_data["visibility"],
+                    story_data["title"],
+                    story_data["description"],
+                    file_data["file"]["resource"],
+                    story_data["is_quick_story"],
+                    story_data["timestamp"]
+                )
+                logging.debug("Python Server Response: 201 - %s", story_created)
+                return make_response(jsonify(story_created), 200)
+            logging.debug("Python Server Response: %s - %s", response.status_code, response.text)
+            return make_response(response.text, response.status_code)
         except ValueError:
+            print('Error happened')
             error = "Unable to handle StoriesResource POST Request"
             logging.error("Python Server Response: 500 - %s", error)
             return ErrorHandler.create_error_response(500, error)
