0af0aca8d93164bd6a7216486eb21bd53dae57d2
gmerino92 <merino.gonza@gmail.com>
2018-06-07 05:37:32 -0300

    Hotfix to solve inconsistency between local and production environments while signing up with Facebook.

--- resources/user_resource.py
+++ resources/user_resource.py
@@ -87,7 +87,7 @@ class FacebookLoginResource(Resource):
                 signup_response = requests.post(SHARED_SERVER_USER_PATH, data=json.dumps(payload), headers=headers)
                 current_app.logger.debug("Shared Server Signup Response: %s - %s", signup_response.status_code, signup_response.text)
 
-                if not signup_response.ok:
+                if not signup_response.ok and signup_response.status_code != 400:
                     current_app.logger.debug("Python Server Response: %s - %s", signup_response.status_code, signup_response.text)
                     return make_response(signup_response.text, signup_response.status_code)
                 else:
