a1dc06d53b9b3e697828b8131ebcad1f0708a96f
gocasey <gonzak6@gmail.com>
2018-06-20 00:39:48 -0300

    Using server token when calling the shared server

--- resources/user_resource.py
+++ resources/user_resource.py
@@ -6,7 +6,7 @@ from flask import request, jsonify, make_response, current_app
 from flask_restful import Resource
 
 from config.shared_server_config import SHARED_SERVER_USER_PATH, SHARED_SERVER_TOKEN_PATH, \
-    SHARED_SERVER_APPLICATION_OWNER
+    SHARED_SERVER_APPLICATION_OWNER, SHARED_SERVER_TOKEN
 from model.user import User, UserNotFoundException
 from resources.error_handler import ErrorHandler
 
@@ -34,7 +34,7 @@ class UsersResource(Resource):
                 "password": user_data["password"],
                 "applicationOwner": SHARED_SERVER_APPLICATION_OWNER
             }
-            headers = {'content-type': 'application/json'}
+            headers = {'content-type': 'application/json', 'Authorization': 'Bearer {}'.format(SHARED_SERVER_TOKEN)}
             signup_response = requests.post(SHARED_SERVER_USER_PATH, data=json.dumps(payload), headers=headers)
             current_app.logger.debug("Shared Server Signup Response: %s - %s", signup_response.status_code,
                                      signup_response.text)
@@ -88,7 +88,7 @@ class FacebookLoginResource(Resource):
                 "password": user_data["username"],
                 "applicationOwner": SHARED_SERVER_APPLICATION_OWNER
             }
-            headers = {'content-type': 'application/json'}
+            headers = {'content-type': 'application/json', 'Authorization': 'Bearer {}'.format(SHARED_SERVER_TOKEN)}
 
             user = User.get_facebook_user(user_data["username"])
 
@@ -178,7 +178,7 @@ class UserLoginResource(Resource):
                 "username": credentials["username"],
                 "password": credentials["password"]
             }
-            headers = {'content-type': 'application/json'}
+            headers = {'content-type': 'application/json', 'Authorization': 'Bearer {}'.format(SHARED_SERVER_TOKEN)}
             response = requests.post(SHARED_SERVER_TOKEN_PATH, data=json.dumps(payload), headers=headers)
             current_app.logger.debug("Shared Server Response: %s - %s", response.status_code, response.text)
             json_response = json.loads(response.text)
