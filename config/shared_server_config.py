c554c26dd089fc7b8078e29734626cc9904be542
gmerino92 <merino.gonza@gmail.com>
2018-06-25 04:55:40 -0300

    App server token config.

--- config/shared_server_config.py
+++ config/shared_server_config.py
@@ -1,8 +1,10 @@
 import os
 
+developmentAppServerToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoxLCJuYW1lIjoibmV3TW9kaWZpZWROYW1lIiwiaXNfYWRtaW4iOmZhbHNlfSwiaWF0IjoxNTI5ODk1MTAwfQ.Ndwy-nWLQs43BImyuqZITFdR3xA2dgYshNP4qAl8iFs'
+
 SHARED_SERVER_URI = 'https://shared-server-stories.herokuapp.com'
 SHARED_SERVER_APPLICATION_OWNER = os.getenv('SHARED_SERVER_NAME', 'newModifiedName')
-APP_SERVER_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoxLCJuYW1lIjoibmV3TW9kaWZpZWROYW1lIiwiaXNfYWRtaW4iOmZhbHNlfSwiaWF0IjoxNTI5ODk1MTAwfQ.Ndwy-nWLQs43BImyuqZITFdR3xA2dgYshNP4qAl8iFs"
+APP_SERVER_TOKEN = os.getenv('APP_SERVER_TOKEN', developmentAppServerToken)
 SHARED_SERVER_TOKEN_VALIDATION_PATH = SHARED_SERVER_URI + '/api/token_check'
 SHARED_SERVER_PING_PATH = SHARED_SERVER_URI + '/api/ping'
 SHARED_SERVER_USER_PATH = SHARED_SERVER_URI + '/api/user'
